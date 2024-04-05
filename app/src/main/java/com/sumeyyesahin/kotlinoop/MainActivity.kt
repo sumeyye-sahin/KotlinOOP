813f8b6f3e1dbf02116dad67b162e12bb885ecbf
Sümeyye Şahin <smyysahin823@gmail.com>
2024-04-05 03:29:36 +0300

    Construct and init added

--- app/src/main/java/com/sumeyyesahin/kotlinoop/MainActivity.kt
+++ app/src/main/java/com/sumeyyesahin/kotlinoop/MainActivity.kt
@@ -7,5 +7,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var myUser = User("John", 45) // User sınıfından myUser adında obje oluşturuldu
+                            // Oluşturulurken constructor çalıştı ve nameInput - ageInput girmek gerekti
+                            // init fonk.da çalıştı
+
+        myUser.name="Justin"
+        myUser.age=76
+
+        println(myUser.age.toString())
+        println(myUser.name)
+
+
+
     }
 }
---
+++ app/src/main/java/com/sumeyyesahin/kotlinoop/User.kt
@@ -0,0 +1,23 @@
+package com.sumeyyesahin.kotlinoop
+
+class User {
+
+    var name : String? = null
+    var age : Int? = null
+
+
+    constructor(nameInput: String, ageInput: Int){
+        this.name=nameInput
+        this.age=ageInput
+        println("User created")
+    }
+
+    init {
+        println("init")
+    }
+
+    fun userFunction(){
+        println("Hi from User class")
+    }
+
+}
